dd20ab0ccc76da8322dc6d80192a53795e324638
josue rojas <josuerojas.rojas@gmail.com>
2019-04-05 21:24:21 -0400

    minor style change

--- src/SiteComponents/UserHistory.js
+++ src/SiteComponents/UserHistory.js
@@ -9,6 +9,10 @@ let stockInfo = [
   {name: 'SNAP', price: '10', amount: '100', date: {month: 'Aug 12', year: '2018'}},
   {name: 'NERV', price: '9', amount: '14', date: {month: 'Sep 5', year: '2018'}},
   {name: 'BPMX', price: '.009', amount: '1004', date: {month: 'Jun 20', year: '2008'}},
+  {name: 'AAPL', price: '100', amount: '10', date: {month: 'Aug 10', year: '2018'}},
+  {name: 'SNAP', price: '10', amount: '100', date: {month: 'Aug 12', year: '2018'}},
+  {name: 'NERV', price: '9', amount: '14', date: {month: 'Sep 5', year: '2018'}},
+  {name: 'BPMX', price: '.009', amount: '1004', date: {month: 'Jun 20', year: '2008'}},
 ];
 
 
