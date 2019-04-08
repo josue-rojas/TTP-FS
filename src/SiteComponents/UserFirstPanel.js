737c465667b646a996b0542e9421a7e04551af8b
josue rojas <josuerojas.rojas@gmail.com>
2019-04-08 19:42:26 -0400

    added fetch stocks to form

--- src/SiteComponents/UserFirstPanel.js
+++ src/SiteComponents/UserFirstPanel.js
@@ -8,14 +8,6 @@ import { withRouter } from "react-router-dom";
 import '../Styles/UserFirstPanel.css';
 import Logo from './Logo';
 
-// sample data for now
-let stockInfo = [
-  {name: 'AAPL', price: '100', amount: '10', date: {month: 'Aug 10', year: '2018'}},
-  {name: 'SNAP', price: '10', amount: '100', date: {month: 'Aug 12', year: '2018'}},
-  {name: 'NERV', price: '9', amount: '14', date: {month: 'Sep 5', year: '2018'}},
-  {name: 'BPMX', price: '.009', amount: '1004', date: {month: 'Jun 20', year: '2008'}},
-];
-
 // one of the props include the user so we won't have to send another request
 class UserFirstPanel extends React.Component {
   constructor(props){
--- src/SiteComponents/UserTrade.js
+++ src/SiteComponents/UserTrade.js
@@ -5,6 +5,7 @@ import Loader from '../Components/Loader';
 import { hasInput } from '../Helpers/InputsCheck';
 import { checkAllInputs, handleOnChange } from '../Helpers/InputFunctions';
 import '../Styles/UserTrade.css';
+import { getStockPrice } from '../Helpers/endpoints';
 
 
 export default class UserTrade extends React.Component {
@@ -19,17 +20,46 @@ export default class UserTrade extends React.Component {
         val: '',
         hasError: false,
       },
+      stockValue: {
+        val: '',
+        hasError: false
+      },
       isLoading: false
     }
     this.checkInput = {
       stockName: hasInput,
       stockAmount: hasInput,
+      stockValue: hasInput,
     }
     this.onInputChange = this.onInputChange.bind(this);
     this.submitForm = this.submitForm.bind(this);
   }
 
   onInputChange(e, inputKey){
+    // fetch values everytime user changes input
+    if(inputKey === 'stockName'){
+      let stockValue = { ...this.state.stockValue };
+      if(e.target.value.length < 1){
+        stockValue.val = '';
+        this.setState({ stockValue: stockValue });
+      }
+      else{
+        let symbol = e.target.value.toUpperCase();
+        getStockPrice(symbol)
+        .then((r)=>{
+          if(r[symbol]){
+            stockValue.val = r[symbol].price+'';
+          }
+          else stockValue.val = 'Stock doesnt exist';
+          this.setState({ stockValue: stockValue });
+
+        })
+        .catch((err)=>{
+          stockValue.val = 'error';
+          this.setState({ stockValue: stockValue });
+        })
+      }
+    }
     let inputState = handleOnChange(e, inputKey, this.state, this.checkInput);
     this.setState({
       [inputKey]: inputState
@@ -54,13 +84,23 @@ export default class UserTrade extends React.Component {
         <TextInput
           title='STOCK NAME'
           placeholder='Name of stock'
-          val={this.state.stockName.val}
+          autocomplete={false}
+          value={this.state.stockName.val}
           hasError={this.state.stockName.hasError}
           onChange={(e) => this.onInputChange(e, 'stockName')}/>
         <TextInput
+          title='VALUE'
+          placeholder='Value of stock (auto)'
+          disabled={true}
+          autocomplete={false}
+          value={this.state.stockValue.val}
+          hasError={this.state.stockValue.hasError}
+          onChange={(e) => console.log('nothing here')}/>
+        <TextInput
           title='AMOUNT'
           placeholder='Amount to buy'
-          val={this.state.stockAmount.val}
+          autocomplete={false}
+          value={this.state.stockAmount.val}
           hasError={this.state.stockAmount.hasError}
           onChange={(e) => this.onInputChange(e, 'stockAmount')}/>
         <Button
